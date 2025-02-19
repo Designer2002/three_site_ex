616d3e928a91bd6c003f2f62e6e780cec4c945bc
Designer2002 <marytun2003@gmail.com>
2025-02-19 21:30:32 +0300

    mini fix

--- script.js
+++ script.js
@@ -10,41 +10,42 @@ container.appendChild(renderer.domElement);
 
 
 
-// var modalS = document.getElementById("modalS");
-// var modalP = document.getElementById("modalP");
-// var modalC = document.getElementById("modalC");
-// var addS = document.getElementById("addS");
-// var addP = document.getElementById("addP");
-// var backS = document.getElementById("backS");
-// var backP = document.getElementById("backP");
-// var backC = document.getElementById("backC");
+var modalS = document.getElementById("modalS");
+var modalP = document.getElementById("modalP");
+var modalC = document.getElementById("modalC");
+var addS = document.getElementById("addS");
+var addP = document.getElementById("addP");
+var addC = document.getElementById("addC");
+var backS = document.getElementById("backS");
+var backP = document.getElementById("backP");
+var backC = document.getElementById("backC");
 
-// const loadModal = function(modal, add) {
-//   add.onclick = function () {
-//     modal.style.display = "block";
-//   };
+const loadModal = function(modal, add) {
+  add.onclick = function () {
+    modal.style.display = "block";
+  };
 
-//   // Закрытие модального окна только при клике вне модального окна
-//   modal.onclick = function(event) {
-//     if (event.target === modal) {
-//       modal.style.display = "none";
-//     }
-//   };
-// }
+  // Закрытие модального окна только при клике вне модального окна
+  modal.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+}
 
-// backS.onclick = function() {
-//   modalS.style.display = "none";
-// }
-// backP.onclick = function() {
-//   modalP.style.display = "none";
-// }
-// backC.onclick = function() {
-//   modalC.style.display = "none";
-// }
+backS.onclick = function() {
+  modalS.style.display = "none";
+}
+backP.onclick = function() {
+  modalP.style.display = "none";
+}
+backC.onclick = function() {
+  modalC.style.display = "none";
+}
 
-// loadModal(modalS, addS);
-// loadModal(modalP, addP);
-// loadModal(modalC, addC);
+loadModal(modalS, addS);
+loadModal(modalP, addP);
+loadModal(modalC, addC);
 
 const startAnimationRight = (entries, observer) => {
   entries.forEach(entry => {
